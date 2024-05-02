61008b98fc79b82fdc3a445ba78f6ab3b2a66420
fedro <151860420+xfdrxndre@users.noreply.github.com>
2024-05-02 14:06:37 +0700

    Update Question & Answer Sales E-Commerce.sql

--- Question & Answer Sales E-Commerce.sql
+++ Question & Answer Sales E-Commerce.sql
@@ -12,24 +12,13 @@ AND
 GROUP BY 
     EXTRACT(MONTH FROM order_date)
 ORDER BY 
-    SUM(after_discount) DESC
-LIMIT 1;
+    SUM(after_discount) DESC;
 
 
 
--- Tabel order_detail
-SELECT column_name
-FROM information_schema.columns
-WHERE table_name = 'order_detail';
-
--- Tabel sku_detail
-SELECT column_name
-FROM information_schema.columns
-WHERE table_name = 'sku_detail';
 
 -- Question 2 --
 -- Selama transaksi pada tahun 2022, kategori apa yang menghasilkan nilai transaksi paling besar?
-
 SELECT 
     sd.category,
     SUM(od.after_discount) AS total_transaksi
@@ -44,16 +33,18 @@ GROUP BY
     sd.category
 ORDER BY 
     total_transaksi 
-DESC
-LIMIT 1;
+DESC;
+
+
 
 -- Question 3 --
 -- Bandingkan nilai transaksi dari masing-masing kategori pada tahun 2021 dengan 2022. Sebutkan kategori apa saja yang mengalami peningkatan dan kategori apa yang mengalami penurunan nilai transaksi dari tahun 2021 ke 2022.
-
 SELECT
     sd.category,
     SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2021 THEN od.after_discount ELSE 0 END) AS total_transaksi_2021,
     SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2022 THEN od.after_discount ELSE 0 END) AS total_transaksi_2022,
+    ROUND(((SUM(CASE WHEN EXTRACT(year from od.order_date) = 2022 THEN od.after_discount end) -
+            (SUM(CASE WHEN EXTRACT(year from od.order_date) = 2021 THEN od.after_discount end))))::DECIMAL,0) AS pertumbuhan,
     CASE
         WHEN SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2022 THEN od.after_discount ELSE 0 END) > SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2021 THEN od.after_discount ELSE 0 END) THEN 'Kenaikan'
         WHEN SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2022 THEN od.after_discount ELSE 0 END) < SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2021 THEN od.after_discount ELSE 0 END) THEN 'Penurunan'
@@ -72,23 +63,13 @@ HAVING
     SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2021 THEN od.after_discount ELSE 0 END) <> 0
     AND SUM(CASE WHEN EXTRACT(YEAR FROM od.order_date) = 2022 THEN od.after_discount ELSE 0 END) <> 0
 ORDER BY
-    sd.category;
+    pertumbuhan DESC;
     
 
 
--- Tabel order_detail
-SELECT column_name
-FROM information_schema.columns
-WHERE table_name = 'order_detail';
-
--- Tabel payment_detail
-SELECT column_name
-FROM information_schema.columns
-WHERE table_name = 'payment_detail';
 
 -- Question 4 --
 -- Tampilkan top 5 metode pembayaran yang paling populer digunakan selama 2022 (berdasarkan total unique order)
-    
 SELECT
 	payment_method,
     COUNT(DISTINCT od.id) as jumlah
@@ -106,9 +87,10 @@ ORDER by
 DESC
 LIMIT 5;
 
+
+
 -- Question 5 --
 -- Urutkan dari ke-5 produk ini berdasarkan nilai transaksinya: 1. Samsung 2. Apple 3. Sony 4. Huawei 5. Lenovo
-
 SELECT
 	CASE
     	WHEN LOWER(sku_name) LIKE '%samsung%' THEN 'Samsung'
@@ -120,10 +102,12 @@ SELECT
     ROUND(SUM(after_discount)::NUMERIC, 0) AS transaksi
 FROM
 	order_detail AS od
-JOIN 
+left JOIN 
 	sku_detail AS sd ON od.sku_id = sd.id
 WHERE
-	is_valid = 1 AND
+	order_date BETWEEN '2022-01-01' and '2022-12-31' AND
+    is_valid = 1 AND
+    sd.category = 'Mobiles & Tablets' AND
     CASE
     	WHEN LOWER(sku_name) LIKE '%samsung%' THEN TRUE
         WHEN LOWER(sku_name) LIKE '%apple%' OR LOWER(sku_name) LIKE '%iphone%' OR LOWER(sku_name) LIKE '%macbook%' THEN TRUE
